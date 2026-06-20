167b046d3285b94f9f4a5bd1a0179da373b1c7fa
AJAY989444 <145103886+AJAY989444@users.noreply.github.com>
2026-06-20 10:27:26 +0000

    Update backend to output generated at 7a8f8ec

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,10 @@ const app = express();
 
 // Middlewares
 
-app.use(cors({origin:"http://localhost:5173", credentials: true}));
+app.use(cors({
+  origin: "https://ai-chat-bot-frontend-zj99.onrender.com",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(morgan("dev")); // for development
