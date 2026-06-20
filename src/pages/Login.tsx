81d88c99d633212c6f1dd678810d813a8712e76c
AJAY989444 <145103886+AJAY989444@users.noreply.github.com>
2026-06-20 10:27:27 +0000

    Update frontend to output generated at 7a8f8ec

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,7 +12,7 @@ import Button from "../components/shared/Button";
 import styles from "./AuthForm.module.css";
 
 import axios from "axios";
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL + "/api";
 axios.defaults.withCredentials = true; // Enable sending credentials (e.g., cookies) in cross-origin requests
 
 import { useAuth } from "../context/context";
